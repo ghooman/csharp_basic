86beed7a7aa4f219e74156458554d09d6cb14dd1
ghooman <mailtoghoo@gmail.com>
2026-05-26 23:50:58 +0900

    push

--- obj/Debug/net8.0/csharp_basic.AssemblyInfo.cs
+++ obj/Debug/net8.0/csharp_basic.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp_basic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e939e1459843b73e260eedb8fb8ce7fa243b0225")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7d09662d8b543521895eb2ce6a80a21569ca90d")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp_basic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp_basic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
